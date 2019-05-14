69516c38ff4fc0842b7adb33ff45d95e58e6ed83
Omar <sr.vmago@gmail.com>
2019-05-14 14:26:59 -0600

    sss
    ssssss

--- COLA/cola.php
+++ COLA/cola.php
@@ -49,7 +49,7 @@
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </body>
-<?php require_once 'footer.php'; ?>
+<?php require_once '../footer.php'; ?>
    
   </html>
         
